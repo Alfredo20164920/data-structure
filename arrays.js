9ab7b36e29556b60a21ae648f6d9f4c59a92b397
Alfredo20164920 <apaz20164920@gmail.com>
2023-06-09 21:22:41 -0600

    Feat: create unshift method

--- arrays.js
+++ arrays.js
@@ -28,14 +28,29 @@ class ProptotypeArray {
             return prevItem;
         }
     }
+
+    unshift(item) {
+        if(!item) return this.length;
+
+        if(this.length !== 0) {
+            for(let i = this.length - 1; i >= 0; i--) {
+                this.data[i + 1] = this.data[i];
+            }
+        }
+
+        this.data[0] = item;
+        this.length++;
+        return this.length;
+    }
 }
 
 const names = new ProptotypeArray();
 
 names.push("Alfredo");
 names.push("Angel");
-names.push("Ana");
+console.log(names.push("Ana"));
 
 console.log(names.pop());
+console.log(names.unshift("Aldo"))
 
 console.log(names.getAll());
